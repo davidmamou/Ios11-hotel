c6609cac85d09be54a80e715f67a3ccf1a72905b
David Mamou <david_mamou@live.fr>
2017-12-13 14:24:14 +0200

    hook up the registration tableVC class

---
+++ HotelManzana/Controllers/RegistrationTableVCTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RegistrationTableVCTableViewController.swift
+//  HotelManzana
+//
+//  Created by David Mamou on 13/12/17.
+//  Copyright © 2017 ronny abraham. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationTableVCTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+
+    
+}
