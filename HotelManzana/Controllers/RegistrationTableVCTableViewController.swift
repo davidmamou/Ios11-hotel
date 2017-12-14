78d3630eab1568b0b627f74ab91f81ffe146a7f1
David Mamou <david_mamou@live.fr>
2017-12-14 16:05:07 +0200

    added stepper and labels in storyboard and create @Boutlet for stepper and Lbl

--- HotelManzana/Controllers/RegistrationTableVCTableViewController.swift
+++ HotelManzana/Controllers/RegistrationTableVCTableViewController.swift
@@ -17,7 +17,30 @@ class RegistrationTableVCTableViewController: UITableViewController {
     @IBOutlet weak var CheckIndatePicker: UIDatePicker!
     @IBOutlet weak var CheckOutLbl: UILabel!
     @IBOutlet weak var CheckOutDatePicker: UIDatePicker!
+    @IBOutlet weak var numberAdultsLbl: UILabel!
+    @IBOutlet weak var numberOfAdultsStepper: UIStepper!
     
+    @IBOutlet weak var numberOfChildrenLbl: UILabel!
+    @IBOutlet weak var numberOfChildrenStepper: UIStepper!
+    
+    
+    
+    
+    
+    let checkInDatePickerIndexPath = IndexPath(row: 1, section: 1)
+    let checkOutDatePickerIndexPath = IndexPath(row: 3, section: 1)
+    var isCheckInDatePickerShown: Bool = false {
+        didSet{
+            CheckIndatePicker.isHidden = !isCheckInDatePickerShown
+        }
+    }
+    
+    var isCheckOutDatePickerShown: Bool = false{
+        didSet{
+            CheckOutDatePicker.isHidden = !isCheckOutDatePickerShown
+            
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,8 +54,7 @@ class RegistrationTableVCTableViewController: UITableViewController {
         CheckIndatePicker.minimumDate = midnightToday
         CheckIndatePicker.date = midnightToday
         
-        CheckOutDatePicker.minimumDate CheckIndatePicker.date.addingTimeInterval(86400)
-        
+       CheckOutDatePicker.minimumDate = CheckIndatePicker.date.addingTimeInterval(86400)
     }
     
     func updateDateViews () {
@@ -50,17 +72,84 @@ class RegistrationTableVCTableViewController: UITableViewController {
         let lastName = lastNameTextField.text ?? ""
         let email = emailTextField.text ?? ""
         
+        let checkInDate = CheckIndatePicker.date
+        let checkOutdate = CheckOutDatePicker.date
+        
         print("first Name:\(firstName)")
         print("Last name:\(lastName)")
         print("email:\(email)")
+        
+        print("check in date:\(checkInDate)")
+        print("check out date:\(checkOutdate)")
+        
+   
     }
     
     @IBAction func datePickerChanged(_ sender: UIDatePicker) {
+        
         updateDateViews()
     }
     
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        /*switch (indexPath.section, indexPath.row){
+            
+        case (checkInDatePickerIndexPath.section, checkInDatePickerIndexPath.row):
+                return 0
+        case (checkOutDatePickerIndexPath.section, checkOutDatePickerIndexPath.row):
+                return 0
+        default:
+                return 44.0
+            
+        }*/
+        
+        switch (indexPath.section, indexPath.row) {
+        case (checkInDatePickerIndexPath.section, checkInDatePickerIndexPath.row):
+            if isCheckInDatePickerShown {
+                return 216.0
+            } else {
+                return 0.0
+            }
+        case (checkOutDatePickerIndexPath.section, checkOutDatePickerIndexPath.row):
+            if isCheckOutDatePickerShown {
+                return 216.0
+            } else {
+                return 0.0
+            }
+        default:
+            return 44.0
+        }
+    }
     
-    
-    
-    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        switch (indexPath.section, indexPath.row) {
+        case (checkInDatePickerIndexPath.section, checkInDatePickerIndexPath.row - 1):
+            if isCheckInDatePickerShown{
+                isCheckInDatePickerShown = false
+            }else if isCheckOutDatePickerShown{
+                isCheckOutDatePickerShown = false
+                isCheckInDatePickerShown = true
+            }else{
+                isCheckInDatePickerShown = true
+            }
+            
+            tableView.beginUpdates()
+            tableView.endUpdates()
+        case (checkOutDatePickerIndexPath.section,
+              checkOutDatePickerIndexPath.row - 1):
+            if isCheckOutDatePickerShown {
+                isCheckOutDatePickerShown = false
+            } else if isCheckInDatePickerShown {
+                isCheckInDatePickerShown = false
+                isCheckOutDatePickerShown = true
+            } else {
+                isCheckOutDatePickerShown = true
+            }
+            tableView.beginUpdates()
+            tableView.endUpdates()
+        default:
+            break
+        }
+    }
 }
